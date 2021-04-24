8daf23abc1ee91928ff63c5aa655cc9de094383c
anand58 <anandt.tiwari12@gmail.com>
2021-04-24 22:53:29 +0530

    Update JpaApplication.java
    First commit to the repo. Tested with a controller.

--- src/main/java/com/demo/jpa/JpaApplication.java
+++ src/main/java/com/demo/jpa/JpaApplication.java
@@ -2,12 +2,22 @@ package com.demo.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("test")
 public class JpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
+    }
+
+    @GetMapping("hello")
+    public String getVal(){
+        return "Hello";
     }
 
 }
